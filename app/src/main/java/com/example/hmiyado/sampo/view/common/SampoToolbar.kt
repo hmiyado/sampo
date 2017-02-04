2f9fb49c300430997c0df2ac547f2c95dd3d910c
miyado <hmiyado@gmail.com>
2017-02-05 01:26:57 +0900

    ツールバーにメニューを追加した

--- app/src/main/java/com/example/hmiyado/sampo/view/common/SampoToolbar.kt
+++ app/src/main/java/com/example/hmiyado/sampo/view/common/SampoToolbar.kt
@@ -1,8 +1,12 @@
 package com.example.hmiyado.sampo.view.common
 
 import android.content.Context
+import android.content.Intent
 import android.support.v7.widget.Toolbar
 import android.util.TypedValue
+import com.example.hmiyado.sampo.R
+import com.example.hmiyado.sampo.view.map.MapActivity
+import com.example.hmiyado.sampo.view.result.ResultActivity
 import org.jetbrains.anko.backgroundColor
 
 /**
@@ -18,6 +22,20 @@ class SampoToolbar(context: Context?) : Toolbar(context) {
             minimumHeight = outVal.density
             context.theme.resolveAttribute(android.R.attr.colorPrimary, outVal, true)
             backgroundColor = outVal.data
+            inflateMenu(R.menu.toolbar_menu)
+            setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.toolbar_menu_map     -> {
+                        context.startActivity(Intent(context, MapActivity::class.java).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+                        true
+                    }
+                    R.id.toolbar_menue_result -> {
+                        context.startActivity(Intent(context, ResultActivity::class.java).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+                        true
+                    }
+                    else                      -> false
+                }
+            }
         }
     }
 
