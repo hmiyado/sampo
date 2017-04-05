1c93da5039313e68624ce2f152e4543c3bbe7d59
miyado <hmiyado@gmail.com>
2017-04-05 22:54:28 +0900

    marker 追加用の floating action button を追加

--- app/src/main/java/com/example/hmiyado/sampo/view/map/ui/MapFragmentUi.kt
+++ app/src/main/java/com/example/hmiyado/sampo/view/map/ui/MapFragmentUi.kt
@@ -2,24 +2,24 @@ package com.example.hmiyado.sampo.view.map.ui
 
 import android.view.View
 import android.widget.LinearLayout
+import com.example.hmiyado.sampo.R
 import com.example.hmiyado.sampo.view.common.sampoToolbar
 import com.example.hmiyado.sampo.view.map.MapFragment
 import com.example.hmiyado.sampo.view.map.custom.compassView
 import com.example.hmiyado.sampo.view.map.custom.mapView
 import com.example.hmiyado.sampo.view.map.custom.scaleView
 import org.jetbrains.anko.*
+import org.jetbrains.anko.design.floatingActionButton
 
 /**
  * Created by hmiyado on 2016/08/08.
  * 地図がみえるFragment
  */
 class MapFragmentUi() : AnkoComponent<MapFragment> {
-    companion object {
-        val toolbarId = View.generateViewId()
-        val mapViewId = View.generateViewId()
-        val compassViewId = View.generateViewId()
-        val scaleViewId = View.generateViewId()
-    }
+    val toolbarId = View.generateViewId()
+    val mapViewId = View.generateViewId()
+    val compassViewId = View.generateViewId()
+    val scaleViewId = View.generateViewId()
 
     override fun createView(ui: AnkoContext<MapFragment>) = with(ui) {
         linearLayout {
@@ -31,20 +31,32 @@ class MapFragmentUi() : AnkoComponent<MapFragment> {
                 mapView {
                     id = mapViewId
                 }.lparams(width = matchParent, height = matchParent)
-                compassView {
-                    id = compassViewId
-                }.lparams(width = dip(100), height = dip(100)) {
-                    alignParentTop()
-                    alignParentLeft()
-                }
-                scaleView {
-                    id = scaleViewId
-                }.lparams(width = dip(200), height = dip(50)) {
-                    alignParentBottom()
-                    alignParentLeft()
+
+                relativeLayout {
+                    compassView {
+                        id = compassViewId
+                    }.lparams(width = dip(100), height = dip(100)) {
+                        alignParentTop()
+                        alignParentLeft()
+                    }
+                    scaleView {
+                        id = scaleViewId
+                    }.lparams(width = dip(200), height = dip(50)) {
+                        alignParentBottom()
+                        alignParentLeft()
+                    }
+                    floatingActionButton(theme = R.style.AppTheme) {
+                        imageResource = R.drawable.ic_add_location_white_24dp
+                    }.lparams {
+                        alignParentBottom()
+                        alignParentEnd()
+                    }
+
+                    lparams {
+                        padding = dip(16)
+                    }
                 }
             }
-
         }
     }
 }
