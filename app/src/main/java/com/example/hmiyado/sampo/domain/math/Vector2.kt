a52c4f3c4bc318808ce88283a4a245f21e8c1d73
miyado <hmiyado@gmail.com>
2017-04-08 16:02:13 +0900

    MarkerView を Marker の Location の直上に表示できるようになった

--- app/src/main/java/com/example/hmiyado/sampo/domain/math/Vector2.kt
+++ app/src/main/java/com/example/hmiyado/sampo/domain/math/Vector2.kt
@@ -7,7 +7,12 @@ data class Vector2(
         val x: Double = 0.0,
         val y: Double = 0.0
 ) {
+    companion object {
+        val ZERO = Vector2(0.0, 0.0)
+    }
+
     constructor(x: Float, y: Float) : this(x.toDouble(), y.toDouble())
+    constructor(x: Int, y: Int) : this(x.toDouble(), y.toDouble())
 
     operator fun plus(other: Vector2): Vector2 = Vector2(x + other.x, y + other.y)
 
